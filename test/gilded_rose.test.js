7dd824f4cc460fc3af2355b0ff72aa8a2b804dbf
Yates101 <alecyates94@gmail.com>
2021-05-06 16:45:15 +0100

    Test added to check Aged Brie quality increases when updateQuality is run

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -25,4 +25,10 @@ describe("Gilded Rose", function() {
     expect(positiveSellIn[0].quality).toEqual(0);
   })
 
+  it("Aged Brie increases in quality the older it gets", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBeGreaterThan(0);
+  });
+
 });
